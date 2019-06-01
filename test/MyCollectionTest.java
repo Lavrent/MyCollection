07dba09b40ef0749112fa70aa6c5c828148bf449
Lavrent <lavrent18@yahoo.com>
2019-06-01 17:49:24 +0400

    My Collection

---
+++ test/MyCollectionTest.java
@@ -0,0 +1,10 @@
+
+class MyCollectionTest<E> {
+
+    public static void main(String[] args) {
+
+        MyCollection<String> collection = new MyCollection<>();
+        collection.add("aaa");
+        collection.toString();
+    }
+}
